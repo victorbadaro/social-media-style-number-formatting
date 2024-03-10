4de8676241883458ff14acebe364d08be34c47c1
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-03-10 12:04:44 -0300

    feat: add the number formatting feature itself

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { useState } from 'react';
 import './styles/global.css';
 
 export function App() {
-  return <h1>Hello, World!</h1>;
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
+  const [number, setNumber] = useState(0);
+  const formattedNumber = formatter.format(number);
+
+  return (
+    <>
+      <input
+        type="number"
+        placeholder="Type a number"
+        onChange={event => setNumber(Number(event.target.value))}
+        value={number}
+      />
+      <p>{formattedNumber}</p>
+    </>
+  );
 }
