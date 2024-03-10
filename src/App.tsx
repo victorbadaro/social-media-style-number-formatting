9a26d45d82d44e2679261956a7ef34b1ced71a5f
Victor Badaró <victor.badaro@fatec.sp.gov.br>
2024-03-10 16:22:18 -0300

    feat: add tailwind and change the entire app styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,20 @@ import { useState } from 'react';
 import './styles/global.css';
 
 export function App() {
-  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
   const [number, setNumber] = useState(0);
+  const formatter = Intl.NumberFormat('en', { notation: 'compact' });
   const formattedNumber = formatter.format(number);
 
   return (
-    <>
+    <div className="bg-zinc-950 h-screen flex items-center justify-center flex-col gap-8">
       <input
+        className="bg-transparent text-zinc-600 border-2 border-zinc-400 rounded p-2"
         type="number"
         placeholder="Type a number"
         onChange={event => setNumber(Number(event.target.value))}
         value={number}
       />
-      <p>{formattedNumber}</p>
-    </>
+      <span className="text-zinc-50 font-bold text-5xl">{formattedNumber}</span>
+    </div>
   );
 }
